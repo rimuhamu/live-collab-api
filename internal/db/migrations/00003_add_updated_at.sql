45caad48e533a1cbc5622df0a74429a80d6e1299
Emir Risyad <emirrsyd@gmail.com>
2025-09-11 14:06:13 +0200

    feat: add missing down migration

--- internal/db/migrations/00003_add_updated_at.sql
+++ internal/db/migrations/00003_add_updated_at.sql
@@ -7,3 +7,13 @@ ALTER TABLE documents
 
 ALTER TABLE events
     ADD COLUMN updated_at TIMESTAMPTZ DEFAULT now();
+
+-- +goose Down
+ALTER TABLE users
+    DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE documents
+    DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE events
+    DROP COLUMN IF EXISTS updated_at;
