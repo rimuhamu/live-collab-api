6f7e617e124ee8bd535cf5d76eb57a6d53d99fe1
Emir Risyad <emirrsyd@gmail.com>
2025-12-30 21:13:54 +0100

    fix: add prefix to env variable url

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,8 +16,8 @@ func LoadConfig() *Config {
 	cfg := &Config{
 		DBUrl:          getEnv("DATABASE_URL", "postgres://collab:collab123@localhost:5432/collabdb?sslmode=disable"),
 		JWTSecret:      getEnv("JWT_SECRET", "vvvsupersecret"),
-		RedisUrl:       getEnv("REDIS_URL", "localhost:6379"),
-		FrontendUrl:    getEnv("FRONTEND_URL", "localhost:3000"),
+		RedisUrl:       getEnv("REDIS_URL", "http://localhost:6379"),
+		FrontendUrl:    getEnv("FRONTEND_URL", "http://localhost:3000"),
 		AllowedOrigins: getEnv("ALLOWED_ORIGINS", "*"),
 	}
 
